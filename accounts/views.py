3543c46f9d041a28685e9a0e23e47e28e9320901
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-06 19:51:58 +0530

    - templates added and tested

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse('Sample!@')
+    return render(request, "accounts/dashboard.html")
