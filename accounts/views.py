7bd53736651c871f6a153cc45992a45d3cd241d6
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-06 20:42:21 +0530

    - add tempaltes and views on different paths as per project requirements

--- accounts/views.py
+++ accounts/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, "accounts/dashboard.html")
+
+
+def products(request):
+    return render(request, "accounts/products.html")
+
+
+def customer(request):
+    return render(request, "accounts/customer.html")
