00d52259687f28c48f89d9f3d5be21b1ad595c88
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-08 07:14:49 +0530

    - add Customer and Product data models and update in admin for superuser access

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=20, null=True)
+    email = models.CharField(max_length=20, null=True)
+    phone_number = models.CharField(max_length=20, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Outdoor', 'Outdoor'),
+    )
+    name = models.CharField(max_length=20, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=20, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=20, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
