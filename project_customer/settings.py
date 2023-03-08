251188353e1463867423fc3c9f3134e1e3a6e97f
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-08 06:24:39 +0530

    - connect with postgres

--- project_customer/settings.py
+++ project_customer/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "project_customer.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": env("DATABASE_NAME"),
+        "USER": env("DATABASE_USER"),
+        "PASSWORD": env("DATABASE_PASS"),
+        "HOST": env("DATABASE_HOST"),
+        "PORT": env("DATABASE_PORT"),
     }
 }
 
