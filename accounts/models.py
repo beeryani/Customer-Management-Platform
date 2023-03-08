e2f8a789c9b7c44478286496d4c5ebfc271006f2
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-08 08:09:54 +0530

    - refactoring changes

--- accounts/models.py
+++ accounts/models.py
@@ -12,6 +12,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=20, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ("Indoor", "Indoor"),
@@ -20,15 +27,9 @@ class Product(models.Model):
     name = models.CharField(max_length=20, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=20, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=20, null=True)
+    description = models.CharField(max_length=20, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self) -> str:
-        return self.name
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=20, null=True)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self) -> str:
         return self.name
@@ -44,7 +45,3 @@ class Order(models.Model):
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, null=True, choices=STATUS)
-    tags = models.ManyToManyField(Tag)
-
-    def __str__(self) -> str:
-        return self.id
