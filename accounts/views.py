9a9a649a153e63218c9879b3af3bfafc534a41f6
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-09 17:08:57 +0530

    - add query sets to render number of customers

--- accounts/views.py
+++ accounts/views.py
@@ -4,15 +4,26 @@ from .models import *
 
 
 def home(request):
-    customers = Customer.objects.all().order_by('-date_created')[:10]
+    customers = Customer.objects.all().order_by("-date_created")[:10]
     orders = Order.objects.all()
-    data = {'customers': customers, 'orders': orders}
+
+    orders_total = orders.count()
+    orders_delivered = orders.filter(status = "Delivered").count()
+    orders_pending = orders.filter(status = "Pending").count()
+
+    data = {
+        "customers": customers,
+        "orders": orders,
+        "orders_total": orders_total,
+        "orders_delivered": orders_delivered,
+        "orders_pending": orders_pending,
+    }
     return render(request, "accounts/dashboard.html", data)
 
 
 def products(request):
     products = Product.objects.all()
-    return render(request, "accounts/products.html", {'products': products})
+    return render(request, "accounts/products.html", {"products": products})
 
 
 def customer(request):
