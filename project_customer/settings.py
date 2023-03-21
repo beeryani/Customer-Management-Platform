ad488901d8b14a416b0b67c5dca8df6e2c91b357
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-21 13:11:23 +0530

    - add django-filters package | extend settings to local

--- project_customer/settings.py
+++ project_customer/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-from pathlib import Path
-import environ
 import os
+from pathlib import Path
+
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
