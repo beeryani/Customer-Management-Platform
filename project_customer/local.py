aa6bac5ab9d490c04e55bd669eda30b08e31df21
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-29 07:46:19 +0530

    - refactor based on linter

--- project_customer/local.py
+++ project_customer/local.py
@@ -15,8 +15,6 @@ from pathlib import Path
 
 import environ
 
-from .settings import *
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
